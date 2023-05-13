42ab1cb8d6a8c3eea0c04732a47d460c1ff09a9a
AndreaMessene <amessene97@gmail.com>
2023-05-13 04:02:08 +0200

    orari e contatti

--- src/components/AContattiAzienda.vue
+++ src/components/AContattiAzienda.vue
@@ -6,14 +6,39 @@ export default {
 </script>
 
 <template>
-    <div class="orari-contatti">
+    <div class="cont">
+        <div class="contatti d-flex justify-content-between">
+            <div class="orario">
+                <i class="fa-solid fa-clock"></i>
+                <span>Open Hours Mon - Sat - 9.00 - 18.00</span>
+            </div>
+            <div class="recapiti">
+                <span><i class="fa-solid fa-phone"></i> +1(305) 1234-5678</span>
+                <span><i class="fa-regular fa-envelope"></i> hello@example.com</span>
+                <i class="fa-brands fa-facebook-f"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-linkedin-in"></i>
+            </div>
+        </div>
 
     </div>
 </template>
 
 <style lang="scss" scoped>
-.orari-contatti {
+.cont {
     background-color: #21333E;
-    height: 30px;
+
+    .contatti {
+        width: 75%;
+        margin: 0 auto;
+        padding: 10px 0;
+    }
+
+    i,
+    span {
+        color: whitesmoke;
+        padding: 0 8px;
+        font-size: 14px;
+    }
 }
 </style>
